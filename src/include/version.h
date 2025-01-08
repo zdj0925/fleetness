811d33e0a2e71c88d90eae21d90f58ccdd9e97d2
zdj925@gmail.com <zdj925@gmail.com>
2025-01-08 16:07:50 +0800

    add json-rpc-server

---
+++ src/include/version.h
@@ -0,0 +1,12 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+
+#define VERSION_STRING "1.0.0"
+
+#define VERSION_STRING_FULL "1.0.0"
+void show_version(const char *file);
+#endif
