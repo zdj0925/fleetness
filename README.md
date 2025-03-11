c8b6033df90ddf7664e1cf684aa2de14049df06b
zdj925@gmail.com <zdj925@gmail.com>
2025-03-11 10:31:57 +0800

    分离rpc功能到单个文件

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 mkdir build
 cmake -S . -B build
 ```
+# run
+```bash
+cmake --build build
+```
+# clean
+```bash
+cmake --build build --target clean
+```
+# test
+```bash
+echo "{\"method\":\"sayHello\"}" | nc localhost 1234
+or
+echo "{\"method\": \"add\", \"params\": [3,4], \"id\": \"SOME_IDENTIFIER\" }" | nc  localhost 1234
+or
+echo "{\"method\":\"exit\"}" | nc localhost 1234
+```
+
---
+++ src/rpcfunc.c
@@ -0,0 +1,65 @@
+
+/*
+ * rpcfunc.c
+ *
+ *  Created on: Oct 9, 2025
+ *      Author: zhaodj
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include "jsonrpc-c.h"
+
+#define PORT 1234                      // the port users will be connecting to
+
+//struct jrpc_server my_server;
+
+cJSON *say_hello (jrpc_context * ctx, cJSON * params, cJSON * id)
+{
+    cJSON *returned = NULL;
+    if (id == NULL)
+    {
+        return cJSON_CreateString ("Hello, i`m fleetness!");
+    }
+    printf ("id: %s\n", id->valuestring);
+
+    //create a new json object
+    returned = cJSON_CreateObject ();
+
+    cJSON_AddStringToObject (returned, "id", "12345");
+    cJSON_AddStringToObject (returned, "method", "say_hello");
+    cJSON_AddStringToObject (returned, "message", "HI, I`m fleetness!");
+    cJSON_AddNumberToObject (returned, "code", 200);
+    cJSON_AddTrueToObject (returned, "success");
+    cJSON_AddNumberToObject (returned, "salt", rand () % 10000);
+    return returned;
+}
+
+cJSON *add (jrpc_context * ctx, cJSON * params, cJSON * id)
+{
+    int i = 0;
+    long int sum = 0;
+    for (i = 0; i < cJSON_GetArraySize (params); i++) 
+    {
+        sum += cJSON_GetArrayItem (params, i)->valueint;
+    }
+
+    return cJSON_CreateNumber (sum);
+}
+
+cJSON *notify (jrpc_context * ctx, cJSON * param, cJSON * id)
+{
+    return NULL;
+}
+
+/////file end/////
